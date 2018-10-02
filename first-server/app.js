558b164af1815d8ff8a0f428824cabc6e8cf9642
Louis Riehl <louisriehl@gmail.com>
2018-10-02 15:17:31 +0000

    can now handle a few different requests

--- first-server/app.js
+++ first-server/app.js
@@ -3,6 +3,15 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 function requestHandler(request, response) {
+  if(request.url == '/') {
+    response.end("Welcome to the homepage!");
+  } else if (request.url == "/about") {
+    response.end("<html><body><h1>This is a test server!</h1></body></html>");
+  } else {
+    response.statusCode = 404;
+    response.end("Oops! There's nothing here!");
+  }
+
   response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
 }
 
